07bd14703e2721548bead2793d5e8049c421f9b2
no234yt <fauzanalaziz345@gmail.com>
2025-10-19 21:19:59 +0800

    Create extrabuttons.lua

---
+++ extrabuttons.lua
@@ -0,0 +1,15 @@
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/no234yt/Chase-thing-test/refs/heads/main/spindash.lua"))()
+end)
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/no234yt/Chase-thing-test/refs/heads/main/emote.lua"))()
+end)
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/no234yt/Chase-thing-test/refs/heads/main/enablejump2.lua"))()
+end)
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/no234yt/Chase-thing-test/refs/heads/main/air-dash.lua"))()
+end)
