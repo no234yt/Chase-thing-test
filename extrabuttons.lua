c373e3893ccbeb9ad02a1416003780abcfde9802
no234yt <fauzanalaziz345@gmail.com>
2025-10-21 06:40:24 +0800

    Update extrabuttons.lua

--- extrabuttons.lua
+++ extrabuttons.lua
@@ -9,3 +9,7 @@ end)
 task.spawn(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/no234yt/Chase-thing-test/refs/heads/main/enablejump2.lua"))()
 end)
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/no234yt/Chase-thing-test/refs/heads/main/spindashability.lua"))()
+end)
